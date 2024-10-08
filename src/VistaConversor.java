3246ec08d39d8b3a201234e7b1bae1a35312a2f3
CR-Best <crosales3103@gmail.com>
2024-10-08 11:05:03 -0600

    Se creo el modelo Conversor.java

---
+++ src/VistaConversor.java
@@ -0,0 +1,10 @@
+package Vista;
+
+import Controlador.ControladorConversor;
+
+public class VistaConversor {
+    public static void main(String[] args) {
+        ControladorConversor controlador = new ControladorConversor();
+        controlador.mostrarMenu(); // Iniciar el menú
+    }
+}
---
+++ src/com/aluracursos/modelo/Conversor.java
@@ -0,0 +1,6 @@
+package com.aluracursos.modelo;
+
+public class Conversor {
+    private final String API_KEY = "43d0222c120dfad97edefcab"; // Reemplaza con tu API Key
+    private final String URL_API = "https://api.exchangerate-api.com/v4/latest/";
+}
